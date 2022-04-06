9c6652f345b0de9a24411e3d029f4e9d0d8ae391
Adam Zerella <1501560+azerella@users.noreply.github.com>
2022-04-06 15:17:19 +0930

    Slight error

--- src/middleware/404.ts
+++ src/middleware/404.ts
@@ -9,17 +9,17 @@ const logger = log.getLogger('NoRouteMiddleware');
  */
 export default function routeNotFoundMiddleware() {
   return async function (ctx: Context, next: Next) {
+    await next();
+
     const { status, body } = ctx;
 
-    logger.debug({ ctx }, 'Caught request for unknown route');
-
     if (status === 404 && body === undefined) {
+      logger.debug({ ctx }, 'Caught request for unknown route');
+
       ctx.status = 404;
       ctx.body = {
         error: 'No route here',
       };
     }
-
-    await next();
   };
 }
